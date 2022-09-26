c5fd37105811b4d28c49f8b500d9484279f3fbfe
pujiarto <pujiarto.ta@gmail.com>
2022-09-26 11:07:54 +0800

    added linear progress indicator

--- app/src/main/java/com/pujiarto/ppidwebview/MainActivity.kt
+++ app/src/main/java/com/pujiarto/ppidwebview/MainActivity.kt
@@ -13,8 +13,7 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -42,7 +41,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Content(){
     Scaffold(
-        topBar = { TopAppBar(title = { Text("PPID Lombok Barat", color = Color.White) }, backgroundColor = MaterialTheme.colors.primary) },
+        topBar = { TopAppBar(
+            title = {
+                Text(
+                    "PPID Lombok Barat",
+                    color = Color.White
+                ) },
+            backgroundColor = MaterialTheme.colors.primary,
+        ) },
         content = { MainContent() }
     )
 }
@@ -52,69 +58,72 @@ fun MainContent(){
 
     var backEnabled by remember { mutableStateOf(false)}
     var webView: WebView? = null
-
-    val visibility = remember { mutableStateOf(true)}
-
+    val visibility = remember { mutableStateOf(false)}
     val url = "https://ppid.lombokbaratkab.go.id/"
-
-    if (visibility.value){
-        CircularProgressIndicator(
-            modifier = Modifier.fillMaxWidth(),
-            strokeWidth = 4.dp
-        )
-    }
-    AndroidView(
-        modifier = Modifier,
-        factory = { context ->
-            WebView(context).apply {
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-            )
-            webViewClient = object : WebViewClient() {
-                override  fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?){
-                    visibility.value = true
-                    backEnabled = view.canGoBack()
-                }
-            }
-                @SuppressLint("SetJavaScriptEnabled")
-                settings.javaScriptEnabled = true
-                loadUrl(url)
-                webView = this
-                visibility.value = false
-
-                webView?.setDownloadListener { url, userAgent, contentDisposition, mimeType, _ ->
-                    val request = DownloadManager.Request(Uri.parse(url))
-                    request.setMimeType(mimeType)
-                    //------------------------COOKIE!!------------------------
-                    val cookies = CookieManager.getInstance().getCookie(url)
-                    request.addRequestHeader("cookie", cookies)
-                    //------------------------COOKIE!!------------------------
-                    request.addRequestHeader("User-Agent", userAgent)
-                    request.setDescription("Downloading file...")
-                    request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType))
-                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-                    request.setDestinationInExternalPublicDir(
-                        Environment.DIRECTORY_DOWNLOADS,
-                        URLUtil.guessFileName(url, contentDisposition, mimeType)
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (visibility.value){
+            LinearProgressIndicator(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(4.dp),
+                color = MaterialTheme.colors.primary,
+                backgroundColor = Color.Gray
+                )
+        }
+        AndroidView(
+            modifier = Modifier,
+            factory = { context ->
+                WebView(context).apply {
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
                     )
-                    val dm = context.getSystemService(DOWNLOAD_SERVICE) as DownloadManager?
-                    dm!!.enqueue(request)
-                    Toast.makeText(
-                        context,
-                        "Downloading File",
-                        Toast.LENGTH_LONG
-                    ).show()
+                    webViewClient = object : WebViewClient() {
+                        override  fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?){
+                            visibility.value = true
+                            backEnabled = view.canGoBack()
+                        }
+                        override fun onPageFinished(view: WebView?, url: String?) {
+                            visibility.value = false
+                        }
+                    }
+                    @SuppressLint("SetJavaScriptEnabled")
+                    settings.javaScriptEnabled = true
+                    loadUrl(url)
+                    webView = this
+                    webView?.setDownloadListener { url, userAgent, contentDisposition, mimeType, _ ->
+                        val request = DownloadManager.Request(Uri.parse(url))
+                        request.setMimeType(mimeType)
+                        //------------------------COOKIE!!------------------------
+                        val cookies = CookieManager.getInstance().getCookie(url)
+                        request.addRequestHeader("cookie", cookies)
+                        //------------------------COOKIE!!------------------------
+                        request.addRequestHeader("User-Agent", userAgent)
+                        request.setDescription("Downloading file...")
+                        request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType))
+                        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                        request.setDestinationInExternalPublicDir(
+                            Environment.DIRECTORY_DOWNLOADS,
+                            URLUtil.guessFileName(url, contentDisposition, mimeType)
+                        )
+                        val dm = context.getSystemService(DOWNLOAD_SERVICE) as DownloadManager?
+                        dm!!.enqueue(request)
+                        Toast.makeText(
+                            context,
+                            "Downloading File",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
                 }
-            }
-    }, update = {
-        webView = it
-    })
-
-    BackHandler(enabled = backEnabled) {
-        webView?.goBack()
+            }, update = {
+                webView = it
+            })
+        BackHandler(enabled = backEnabled) {
+            webView?.goBack()
+        }
     }
-
 }
 
 
